559789b8cb6c254343c63cd9fef386af0fe5e108
piniu10 <matpiniarski@gmail.com>
2024-03-25 12:37:18 +0100

    Config 25.03.24

--- Source/HitProject2D/HitProject2D.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HitProject2D.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HitProject2D, "HitProject2D" );
--- Source/HitProject2D/HitProject2DGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "HitProject2DGameModeBase.h"
-
